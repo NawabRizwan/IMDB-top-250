bcbb892bae9f6bb27b919464b46b39c3036e5584
Rizwan Nawab <nawabrizwan7@gmail.com>
2017-06-13 17:19:16 +0530

    minor changes

--- top-250-imdb.py
+++ top-250-imdb.py
@@ -2,21 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-s = int(input("press 1 for movies, 2 for TV series"))
+#get user input for donwloading list of movies or TV series
+s = int(input("press 1 for movies, 2 for TV series:	"))
 r = ""
 
+if s!=1 and s!=2:
+	print("Wrong input, exiting")
+	exit()
+
+#If user selects TV series append the following text in the link
 if s == 2:
 	r = "tv/"
+
 
 response = requests.get("http://www.imdb.com/chart/top"+r)
 soup = BeautifulSoup(response.content, 'html.parser')
 
+#get the titles of the movies or TV series and store in titlesls
 titlesls = []
 movies_class = soup.find_all('td', class_='titleColumn')
 for movie in movies_class:
     movie_name = movie.find('a').get_text()
     titlesls.append(movie_name)
 
+#get the year of release of the movies or TV series and store in yearsls
 yearsls = []
 for movie in movies_class:
     year = movie.find('span').get_text()
@@ -24,6 +33,7 @@ for movie in movies_class:
     yr = int(yr)
     yearsls.append(yr)
 
+#get the ratings of the movies or TV series and store in ratingsls
 ratingsls = []
 ratings = soup.find_all('td', class_ = 'ratingColumn imdbRating')
 for rating in ratings:
@@ -31,13 +41,15 @@ for rating in ratings:
     r = float(r)
     ratingsls.append(r) 
 
+#zip all the data in a list
 Data = list(zip(titlesls,yearsls,ratingsls))
 df = pd.DataFrame(data = Data, columns=['Name', 'Year of release','Rating'])
 
+#store the .csv file
 if s == 1:
-	df.to_csv('top-250-movies.csv',index=False,header=False)
+	df.to_csv('top-250-movies.csv',index=False,header=True)
 else:
-	df.to_csv('top-250-series.csv',index=False,header=False)		
+	df.to_csv('top-250-series.csv',index=False,header=True)		
 
 
 
